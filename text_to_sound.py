eb83560d6b5543e5fe4bc101277e6e4ef0b21231
Deepraj Rakshit <deep.papai07@gmail.com>
2020-08-15 02:51:18 +0530

    This fixes #2
    Changing the audio player to pygame player from system default payer

--- text_to_sound.py
+++ text_to_sound.py
@@ -6,7 +6,7 @@ def speak(text):
     with io.BytesIO() as file:
         gTTS(text=text,lang="en").write_to_fp(file)
         file.seek(0)
-        pygame.mixer.init()
+        pygame.mixer.init() #Using pygame player to play the audio instead of using system player
         pygame.mixer.music.load(file)
         pygame.mixer.music.play()
         while pygame.mixer.music.get_busy():
